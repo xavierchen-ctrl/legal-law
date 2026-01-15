c42cea5104383df5903f24149a4e34140c02c0e3
caesarliu617 <danny75617@gmail.com>
2026-01-15 17:23:27 +0800

    fix: remove deprecated instrumentationHook

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // Required for instrumentation in some versions, harmless if mainstraemed
-    instrumentationHook: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
